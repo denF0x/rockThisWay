356e32ce7a2acaa62e7410777269113859b3c5ab
Denis <fox-94.09@yandex.ru>
2017-02-08 21:24:07 +0200

    Just Goblin

---
+++ Game/TextRPG/src/Goblin.java
@@ -0,0 +1,11 @@
+package Game.TextRPG.src;
+
+/**
+ * Created by Денис on 08.02.2017.
+ */
+public class Goblin extends Monster {
+    public Goblin(String _charClass, String _name, int _strength, int _dexterity, int _endurance) {
+        super(_charClass, _name, _strength, _dexterity, _endurance);
+    }
+    public void walk(){}}
+
